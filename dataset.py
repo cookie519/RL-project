39063dd7eed1ed69f6f3689ac6027d01e022ffb9
Jiaxin Xiao <95730453+cookie519@users.noreply.github.com>
2024-04-04 15:35:47 -0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,1 +1,20 @@
-# load dataset
+# dataset for d4rl
+import d4rl
+import gym
+import torch
+
+class D4RL_dataset(torch.utils.data.Dataset):
+    def __init__(self, args):
+        pass
+
+    def __getitem(self, index):
+        pass
+
+    def shuffle_data(self):
+        pass
+
+    def sample(self, batch_size):
+        pass
+    
+    def __len__(self):
+        pass
