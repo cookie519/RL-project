3b2562b09da4da1b724fe13eb29f37d3a6283e1d
yingqing001 <154562310+yingqing001@users.noreply.github.com>
2024-04-04 20:21:42 -0400

    Update model.py

--- model.py
+++ model.py
@@ -60,6 +60,16 @@ class ValueFunction(nn.Module):
 
 
 class DiracPolicy(nn.Module):
+    """
+    A policy network approximating the policy function π(s) -> a, mapping state inputs to actions.
+    Utilizes a tanh activation on the output to ensure the action values are within a normalized range.
+    
+    Args:
+    - state_dim (int): Dimensionality of the state space.
+    - action_dim (int): Dimensionality of the action space.
+    - num_hidden_layers (int): Number of hidden layers in the network.
+    - hidden_dim (int): Sizer of hidden layers in the network.
+    """
     def __init__(self, state_dim, action_dim, num_hidden_layers=2, hidden_dim=256):
         super(DiracPolicy, self).__init__()
         dims = [state_dim] + [hidden_dim] * num_hidden_layers + [action_dim]
@@ -73,6 +83,15 @@ class DiracPolicy(nn.Module):
 
 
 class MLPResNetBlock(nn.Module):
+    """
+    A block of the MLPResNet architecture, implementing a residual connection around two linear layers.
+    
+    Args:
+    - features (int): Number of features in the input and output of the block.
+    - act (callable): Activation function to be applied after the first linear layer.
+    - dropout_rate (float, optional): Dropout rate applied after the activation function. If None or 0, dropout is not used.
+    - use_layer_norm (bool): Flag indicating whether to use Layer Normalization before the activation function.
+    """
     def __init__(self, features, act=F.relu, dropout_rate=None, use_layer_norm=False):
         super(MLPResNetBlock, self).__init__()
         self.features = features
@@ -100,6 +119,22 @@ class MLPResNetBlock(nn.Module):
 
 
 class MLPResNet(nn.Module):
+    """
+    Defines a ResNet-like architecture with multiple MLPResNetBlocks.
+    
+    Args:
+        num_blocks (int): The number of MLPResNet blocks to include in the model. Each block includes
+                          a residual connection around its layers.
+        input_dim (int): The dimensionality of the input features.
+        out_dim (int): The dimensionality of the output.
+        dropout_rate (float, optional): The dropout rate applied within each MLPResNet block. If None or 0,
+                                        no dropout is applied. 
+        use_layer_norm (bool): Flag indicating whether to apply Layer Normalization within each block.
+        hidden_dim (int): The number of units in the hidden layers within each block.
+        activations (callable): The activation function to apply after each linear layer except for the final
+                                output layer. 
+
+    """
     def __init__(self, num_blocks, input_dim, out_dim, dropout_rate=None, use_layer_norm=False, hidden_dim=256, activations=F.relu):
         super(MLPResNet, self).__init__()
         self.num_blocks = num_blocks
@@ -128,8 +163,6 @@ class MLPResNet(nn.Module):
         return x
 
 
-class ScoreNet_IDQL(nn.Module):
-
 
 class GaussianFourierProjection(nn.Module):
     """Gaussian random features for encoding temporal information.
@@ -146,6 +179,42 @@ class GaussianFourierProjection(nn.Module):
         """Applies the Gaussian Fourier projection to the input tensor."""
         x_proj = x.unsqueeze(dim=-1) * self.W.unsqueeze(dim=0) * 2 * np.pi
         return torch.cat([torch.sin(x_proj), torch.cos(x_proj)], dim=-1)
+
+
+
+class ScoreNet_IDQL(nn.Module):
+    """
+    Implements a Score Network for Inverse Dynamics Q-Learning (IDQL), integrating an embedding layer for time conditioning.
+
+    Args:
+        input_dim (int): The dimensionality of the input state space.
+        output_dim (int): The dimensionality of the output action space.
+        marginal_prob_std (float): The standard deviation of the marginal probability distribution of the
+                                   input states. 
+        embed_dim (int, optional): The dimensionality of the embedding for the time variable, used in the
+                                   Gaussian Fourier Projection layer. Defaults to 64.
+        args: other arguments.
+    """
+    def __init__(self, input_dim, output_dim, marginal_prob_std, embed_dim=64, args=None):
+        super().__init__()
+        self.output_dim = output_dim
+        self.embed = nn.Sequential(GaussianFourierProjection(embed_dim=embed_dim))
+        self.device = args.device
+        self.marginal_prob_std = marginal_prob_std
+        self.args=args
+
+        # Main network with residual blocks
+        self.main = MLPResNet(args.actor_blocks, input_dim, output_dim, dropout_rate=0.1, use_layer_norm=True, hidden_dim=256, activations=nn.Mish())
+
+        # Conditional model for embedding the time variable
+        self.cond_model = mlp([64, 128, 128], activation=nn.Mish, output_activation=None)
+
+    def forward(self, x, t, condition):
+        embed = self.cond_model(self.embed(t))
+        all = torch.cat([x, condition, embed], dim=-1)
+        h = self.main(all)
+        return h
+         
 
 
 
