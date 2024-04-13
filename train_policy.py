6dfb8d17d28748a2708bee4a9683934616eea09e
yingqing001 <154562310+yingqing001@users.noreply.github.com>
2024-04-13 14:13:19 -0400

    Update train_policy.py

--- train_policy.py
+++ train_policy.py
@@ -12,7 +12,7 @@ from dataset import D4RL_dataset
 from SRPO_model import SRPO
 from utils import get_args, marginal_prob_std, parallel_simple_eval_policy
 
-def train_policy(args, score_model, data_loader, start_epoch=0):
+def train_policy(args, srpo_policy, data_loader, start_epoch=0):
     n_epochs = args.n_policy_epochs
     tqdm_epoch = tqdm.trange(start_epoch, n_epochs)
     evaluation_interval = 2
@@ -21,14 +21,14 @@ def train_policy(args, score_model, data_loader, start_epoch=0):
         num_items = 0
         for _ in range(10000):  
             data = data_loader.sample(args.policy_batchsize)
-            loss = score_model.update_SRPO_policy(data)  
+            loss = srpo_policy.update_SRPO_policy(data)  
             avg_loss += loss  # Corrected to accumulate the returned loss
             num_items += 1
         avg_loss /= num_items  # Calculate mean loss outside the loop for efficiency
         tqdm_epoch.set_description(f'Average Loss: {avg_loss:.5f}')
         
         if (epoch % evaluation_interval == 0) or (epoch == n_epochs - 1):  # Evaluating on the first and last epoch, and periodically
-            mean, std = parallel_simple_eval_policy(score_model.SRPO_policy.select_actions, args.env, seed = 0)
-            args.run.log({"eval/rew_deter": mean, "info/policy_q": score_model.SRPO_policy.q.detach().cpu().numpy(), 
-                          "info/lr": score_model.SRPO_policy_optimizer.state_dict()['param_groups'][0]['lr']}, step=epoch+1)
+            mean, std = parallel_simple_eval_policy(srpo_policy.SRPO_policy.select_actions, args.env, seed = 0)
+            args.run.log({"eval/rew_deter": mean, "info/policy_q": srpo_policy.SRPO_policy.q.detach().cpu().numpy(), 
+                          "info/lr": srpo_policy.SRPO_policy_optimizer.state_dict()['param_groups'][0]['lr']}, step=epoch+1)
 
